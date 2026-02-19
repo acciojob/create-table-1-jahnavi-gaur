a5cfbc0d68e855d18f04b71687e328d243b6db97
jahnavi-gaur <121301783+jahnavi-gaur@users.noreply.github.com>
2026-02-19 08:23:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+let table = document.querySelector('#sampleTable')
+
 function insert_Row() {
     //Write your code here
-  
-  
+	let tr = document.createElement('tr')
+	let td1 = document.createElement('td')
+	td1.textContent = 'New Cell1'
+	let td2 = document.createElement('td')
+	td2.textContent = 'New Cell2'
+	tr.append(td1,td2)
+	table.prepend(tr)
 }
