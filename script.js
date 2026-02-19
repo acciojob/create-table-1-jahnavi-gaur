633bcad64efde0fb9065a0382ff1ad188014c3ee
jahnavi-gaur <121301783+jahnavi-gaur@users.noreply.github.com>
2026-02-19 08:30:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let table = document.querySelector('#sampleTable')
 
 function insert_Row() {
     //Write your code here
-	let tr = document.createElement('tr')
-	let td1 = document.createElement('td')
-	td1.textContent = 'New Cell1'
-	let td2 = document.createElement('td')
-	td2.textContent = 'New Cell2'
-	tr.append(td1,td2)
-	table.prepend(tr)
+	let row = table.insertRow(0); // insert at top
+    
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+
+    cell1.textContent = "New Cell1";
+    cell2.textContent = "New Cell2";
 }
